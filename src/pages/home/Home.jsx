c180fd6d276ad85a1b818680885071a747be6ce9
Giovanny Angel Curahua Albujar <gcurahuaa@gsc.pe>
2024-01-23 18:48:06 -0500

    avanze 2301

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,12 @@ import './home.scss'
 function Home() {
   return (
     <div className='home'>
-        HOME
+        <div className='box box1'>BOX1</div>
+        <div className='box box2'>BOX2</div>
+        <div className='box box3'>BOX3</div>
+        <div className='box box4'>BOX4</div>
+        <div className='box box5'>BOX5</div>
+        <div className='box box6'>BOX6</div>
     </div>
   )
 }
